5df19a8599a6fed5c98d1873b85ba52c88ba0640
Sephora <seph156@gmail.com>
2017-12-27 11:26:45 -0800

    refactored calculator

--- lesson_2/calculate.rb
+++ lesson_2/calculate.rb
@@ -3,27 +3,67 @@
 # perform the operation on the two numbers
 # output the result
 
-Kernel.puts("Please give me a number")
-number_1 = Kernel.gets().chomp()
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
-Kernel.puts("Please give me a second number")
-number_2 = Kernel.gets().chomp()
+def valid_number?(num)
+  num.to_i != 0
+end
 
-Kernel.puts("Choose an operation to perform.")
-operator = Kernel.gets().chomp()
+def get_number(target_number)
+  number = ''
+  loop do
+    prompt("What's the #{target_number} number?")
+    number = Kernel.gets().chomp()
+
+    if valid_number?(number)
+      break
+    else
+      prompt("Oops, that's not a valid number. Please try again.")
+    end
+  end
+  number
+end
 
 def operate(a, b, operator)
   case operator
   when "add"       then a + b
-  when "divide"    then a.to_f / b.to_f
   when "multiply"  then a * b
   when "subtract"  then a - b
-  when "remainder" then a % b
+  when "divide"    then a.to_f / b.to_f
   else
-    Kernel.puts("Invalid input. Please type 'add', 'subtract','multiply','subtract', or 'remainder'.")
-    new_operator = Kernel.gets().chomp()
-    operate(a, b, new_operator)
+    prompt("That is not a valid input.")
+    prompt("Please type 'add', 'multiply', 'subtract', or 'divide'.")
+    operator = Kernel.gets().chomp()
+    operate(a, b, operator)
   end
 end
 
-Kernel.puts(operate(number_1.to_i, number_2.to_i, operator))
+# begin program
+prompt("Welcome to Calculator!")
+name = ''
+loop do
+  prompt("What is your name?")
+  name = Kernel.gets().chomp()
+  if name.empty?
+    prompt("Please give me a valid name :)")
+  else
+    break
+  end
+end
+prompt("Nice to meet you, #{name}")
+
+loop do #main loop
+  number1 = get_number("first")
+  number2 = get_number("second")
+
+  prompt("Choose an operation to perform. ('add', 'multiply', 'subtract', or 'divide')")
+  operator = Kernel.gets().chomp()
+
+  prompt("The result is #{operate(number1.to_i, number2.to_i, operator)}")
+
+  prompt("Would you like to make another calculation? (Y for yes)")
+  another = Kernel.gets().chomp()
+  break unless another.downcase.start_with?("y")
+end
