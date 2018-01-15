a837186f1fa5c68a7f72f4621a4c1f1ca734f1ad
Sephora <seph156@gmail.com>
2018-01-15 14:48:56 -0800

    twenty-one with bonus

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
-BUST = 22
+MAX_TOTAL_VALUE = 21
+DEALER_MAX = 17
+BASE_VALUE = 10
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -24,6 +26,14 @@ def initialize_hand
   new_hand
 end
 
+def initialize_scoreboard
+  new_scoreboard = {
+    'player' => 0,
+    'dealer' => 0
+  }
+  new_scoreboard
+end
+
 def initialize_game!(deck, hands)
   2.times { deal_card!(deck, hands, 'player') }
   2.times { deal_card!(deck, hands, 'dealer') }
@@ -37,7 +47,7 @@ def deal_card!(deck, dealt_cards, player)
 end
 
 def bust?(total)
-  total >= BUST
+  total > MAX_TOTAL_VALUE
 end
 
 def calculate_card_value(card)
@@ -51,7 +61,7 @@ end
 
 def calculate_ace_value(aces, total)
   aces.pop
-  total += total <= (10 - aces.size) ? 11 : 1
+  total += total <= (BASE_VALUE - aces.size) ? 11 : 1
   aces.empty? ? total : calculate_ace_value(aces, total)
 end
 
@@ -61,7 +71,7 @@ def calculate_total_value(hand)
     hand.map { |card| calculate_card_value(card.at(1)) }.reduce(:+)
   else
     no_aces = hand.select { |card| card.at(1) != 'ace' }
-    sum = no_aces.map { |card| calculate_card_value(card.at(1)) }.reduce(:+)
+    sum = no_aces.map { |card| calculate_card_value(card.at(1)) }.reduce(0, :+)
     calculate_ace_value(aces, sum)
   end
 end
@@ -77,8 +87,8 @@ def display_cards(hand, person_has)
   puts "\n"
 end
 
-def display_busted(winner, loser)
-  puts "#{loser} bust! #{winner} Wins."
+def display_busted(other_person, loser)
+  puts "#{loser} Busted! #{other_person} Wins!"
 end
 
 def find_winner(player, dealer)
@@ -101,7 +111,6 @@ def declare_current_score(person, score)
 end
 
 def declare_winner(player, dealer)
-  system('clear') || system('cls')
   declare_current_score("Player", player)
   declare_current_score("Dealer", dealer)
   winner = find_winner(player, dealer)
@@ -121,10 +130,39 @@ def play_again?
   answer.downcase.start_with?('y') ? true : false
 end
 
+def keep_score(winner, scorebrd)
+  scorebrd[winner.downcase] += 1 unless winner == 'tie'
+end
+
+def five?(scorebrd)
+  scorebrd.values.include?(5)
+end
+
+def find_winner_of_five(scorebrd)
+  scorebrd.key(5)
+end
+
+def display_games_won(scorebrd)
+  puts_with_newline("You have won #{scorebrd['player']} games. " \
+                    "The dealer has won #{scorebrd['dealer']} games.")
+end
+
+def declare_grand_winner(winner)
+  puts_with_newline("#{winner.capitalize} has won 5 games and is" \
+                    " the Grand Winner!")
+end
+
+def end_match!(scorebrd)
+  declare_grand_winner(find_winner_of_five(scorebrd))
+  scorebrd['player'] = 0
+  scorebrd['dealer'] = 0
+end
+
 system('clear') || system('cls')
 puts "Welcome to the game of Twenty-One."
-puts_with_newline("Try to make the total value of your cards " \
-                  "as close to 21 as you can without going over.")
+puts_with_newline("Try to make the total value of your cards as " \
+                  "close to #{MAX_TOTAL_VALUE} as you can without going over.")
+scoreboard = initialize_scoreboard
 
 loop do
   puts_with_newline(prompt("Beat the dealer's hand without going bust!"))
@@ -160,7 +198,7 @@ loop do
       puts_with_newline("You chose to stay!")
       break
     else
-      prompt("Oops. That wasn't a valid option. Try again.")
+      puts_with_newline("That isn't a valid option. Try again.")
     end
   end
 
@@ -169,6 +207,10 @@ loop do
     display_cards(hands['player'], "You have")
     declare_current_score("Player", players_total)
     display_busted("Dealer", "You're")
+
+    scoreboard['dealer'] += 1
+    display_games_won(scoreboard)
+    end_match!(scoreboard) if five?(scoreboard)
     play_again? ? next : break
   end
 
@@ -178,10 +220,10 @@ loop do
 
   loop do
     display_cards(hands['dealer'], "Dealer has")
+
+    break if dealers_total >= DEALER_MAX
+
     declare_current_score("Dealer", dealers_total)
-
-    break if dealers_total >= 17
-
     deal_card!(deck, hands, 'dealer')
     puts_with_newline("Dealer hands itself a card.")
     dealers_total = calculate_total_value(hands['dealer'])
@@ -192,10 +234,17 @@ loop do
     display_cards(hands['dealer'], "Dealer has")
     declare_current_score("Dealer", dealers_total)
     display_busted("Player", "Dealer's")
+
+    scoreboard['player'] += 1
+    display_games_won(scoreboard)
+    end_match!(scoreboard) if five?(scoreboard)
     play_again? ? next : break
   end
 
   declare_winner(players_total, dealers_total)
+  keep_score(find_winner(players_total, dealers_total), scoreboard)
+  display_games_won(scoreboard)
+  end_match!(scoreboard) if five?(scoreboard)
   break unless play_again?
 end
 
